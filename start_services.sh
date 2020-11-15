b0f36fcdc8aa69b825f21126c3618b99fcb7e084
Nick Banner <nickbanner007@gmail.com>
2020-11-15 19:37:01 +0530

    second commit

--- start_services.sh
+++ start_services.sh
@@ -1,4 +1,4 @@
 cd app/
 # Start rasa server with nlu model
 rasa run actions
-CMD $(echo “rasa run -p $PORT -m models --credentials credentials.yml --enable-api --log-file out.log --endpoints endpoints.yml” | sed ‘s/=//’)
+rasa run -p $PORT -m models --credentials credentials.yml --enable-api --log-file out.log --endpoints endpoints.yml” | sed ‘s/=//’
